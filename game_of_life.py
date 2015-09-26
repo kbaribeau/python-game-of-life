e100497f182ef221351065c3bdeb0e4ec8b9b311
colleenjhardie <c.hardie@gmail.com>
2015-09-26 15:41:55 -0600

    Demonstrate odd 1x1 bug.

--- game_of_life.py
+++ game_of_life.py
@@ -1,31 +1,54 @@
 # Implementation of Conway's Game of Life
 # https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
-class GameOfLife:
-    def will_be_alive_in_next_generation(self, is_cell_alive, num_alive_neighbours):
+
+class GameOfLife(object):
+
+    def will_cell_be_alive_in_next_generation(self, is_cell_alive, num_alive_neighbours):
+        """
+        Determine whether a given cell state will be alive given its number of alive neighbours
+        """
         if is_cell_alive:
             return num_alive_neighbours == 2 or num_alive_neighbours == 3
         else:
             return num_alive_neighbours == 3
 
-    def count_neighbours(self, grid, coords):
-        def normalize_coords(x, y):
-            maxx = len(grid[0])
-            maxy= len(grid)
-            if x == maxx:
+    def count_alive_neighbours(self, grid, coordinates):
+        """
+        Given a grid and a set of coordinates, determine the number of live neighbors for the given cell
+        """
+
+        def normalize_coordinates(x, y):
+            """
+            Account for wrapping of the grid.  E.g., the neighbour to a cell on the far right of the grid is the cell on
+            the far left of the grid.  Similarly, the neighbour to a cell on the far bottom of the grid is the cell on
+            the far top of the grid.
+            """
+            max_x = len(grid[0])
+            max_y = len(grid)
+            if x == max_x:
                 x = 0
-            if y == maxy:
+            if y == max_y:
                 y = 0
-            return (x, y)
+            return x, y
 
-        x, y = coords
-        result = 0
-        for neighbourx in range(x-1, x+2):
-            for neighboury in range(y-1, y+2):
-                if (x, y) != (neighbourx, neighboury):
-                    neighbourx, neighboury = normalize_coords(neighbourx, neighboury)
-                    result += grid[neighbourx][neighboury]
+        x, y = coordinates
+        alive_neighbours = 0
+        for neighbour_x in range(x-1, x+2):
+            for neighbour_y in range(y-1, y+2):
+                if (x, y) != (neighbour_x, neighbour_y):
+                    neighbour_x, neighbour_y = normalize_coordinates(neighbour_x, neighbour_y)
+                    alive_neighbours += grid[neighbour_x][neighbour_y]
 
-        return result
+        return alive_neighbours
 
-
+# Fix problem with 1x1 grid neighbour counting before worrying about this
+#     def determine_next_grid(self, input_grid):
+#         max_x = len(input_grid[0])
+#         max_y = len(input_grid)
+#         output_grid = [[0 for output_x in range(max_x)] for output_y in range(max_y)]
+#         for x in range(0, max_x):
+#             for y in range(0, max_y):
+#                 num_alive_neighbours = self.count_alive_neighbours(input_grid, (x, y))
+#                 is_alive = self.will_cell_be_alive_in_next_generation(input_grid[x][y], num_alive_neighbours)
+#                 output_grid[x][y] = is_alive
--- game_of_life_test.py
+++ game_of_life_test.py
@@ -7,91 +7,109 @@ class TestGameOfLife(unittest.TestCase):
 
     #Live Cells
     def test_live_cell_with_zero_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 0))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 0))
 
     def test_live_cell_with_one_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 1))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 1))
 
     def test_live_cell_with_two_live_neighbours_stays_alive(self):
-        self.assertTrue(self.gol.will_be_alive_in_next_generation(True, 2))
+        self.assertTrue(self.gol.will_cell_be_alive_in_next_generation(True, 2))
 
     def test_live_cell_with_three_live_neighbours_stays_alive(self):
-        self.assertTrue(self.gol.will_be_alive_in_next_generation(True, 3))
+        self.assertTrue(self.gol.will_cell_be_alive_in_next_generation(True, 3))
 
     def test_live_cell_with_four_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 4))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 4))
 
     def test_live_cell_with_five_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 5))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 5))
 
     def test_live_cell_with_six_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 6))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 6))
 
     def test_live_cell_with_seven_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 7))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 7))
 
     def test_live_cell_with_eight_live_neighbours_dies(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(True, 8))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(True, 8))
 
     #Dead Cells
     def test_dead_cells_with_zero_live_neighbours_stay_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 0))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 0))
 
     def test_dead_cells_with_one_live_neighbour_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 1))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 1))
 
     def test_dead_cells_with_two_live_neighbours_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 2))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 2))
 
     def test_dead_cell_with_three_live_neighbours_comes_to_life(self):
-        self.assertTrue(self.gol.will_be_alive_in_next_generation(True, 3))
+        self.assertTrue(self.gol.will_cell_be_alive_in_next_generation(True, 3))
 
     def test_dead_cell_with_four_live_neighbours_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 4))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 4))
 
     def test_dead_cell_with_five_live_neighbours_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 5))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 5))
 
     def test_dead_cell_with_six_live_neighbours_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 6))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 6))
 
     def test_dead_cell_with_seven_live_neighbours_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 7))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 7))
 
     def test_dead_cell_with_three_live_neighbours_stays_dead(self):
-        self.assertFalse(self.gol.will_be_alive_in_next_generation(False, 8))
+        self.assertFalse(self.gol.will_cell_be_alive_in_next_generation(False, 8))
 
 
 class LiveNeighbourCounterTests(unittest.TestCase):
     gol = GameOfLife()
 
-    def test_dead_grid(self):
+    def test_every_cell_of_dead_grid_has_zero_live_neighbours(self):
         grid = [[0,0,0],
                 [0,0,0],
                 [0,0,0]]
-        self.assertEquals(0, self.gol.count_neighbours(grid, (0,0)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (0,1)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (0,2)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (1,0)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (1,1)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (1,2)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (2,0)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (2,1)))
-        self.assertEquals(0, self.gol.count_neighbours(grid, (2,2)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (0,0)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (0,1)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (0,2)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (1,0)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (1,1)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (1,2)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (2,0)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (2,1)))
+        self.assertEquals(0, self.gol.count_alive_neighbours(grid, (2,2)))
 
     def test_zero_zero_has_eight_live_neighbours(self):
         grid = [[1,1,1],
                 [1,1,1],
                 [1,1,1]]
-        self.assertEquals(8, self.gol.count_neighbours(grid, (0,0)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (0,1)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (0,2)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (1,0)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (1,1)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (1,2)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (2,0)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (2,1)))
-        self.assertEquals(8, self.gol.count_neighbours(grid, (2,2)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (0,0)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (0,1)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (0,2)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (1,0)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (1,1)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (1,2)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (2,0)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (2,1)))
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (2,2)))
+
+    def test_single_alive_cell_grid_has_eight_live_neighbours(self):
+        grid = [[1]]
+        self.assertEquals(8, self.gol.count_alive_neighbours(grid, (0, 0)))
+
+# Fix problem with 1x1 grid neighbour counting before worrying about this
+# class DetermineNextGridTests(unittest.TestCase):
+#     gol = GameOfLife()
+#
+#     def test_single_alive_cell_grid_returns_identical_grid(self):
+#         input_grid = [[1]]
+#         actual_grid = self.gol.determine_next_grid(input_grid)
+#         self.assertEqual(input_grid, actual_grid)
+#
+#     def test_single_dead_cell_grid_returns_identical_grid(self):
+#         input_grid = [[0]]
+#         actual_grid = self.gol.determine_next_grid(input_grid)
+#         self.assertEqual(input_grid, actual_grid)
 
 
 if __name__ == '__main__':
